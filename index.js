8738b9ee249e26b5bf2961b8e38083d9c0d9dbc6
san <sansamaa0@gmail.com>
2023-02-23 12:08:02 +0300

    h.w

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(gifts,text) {
+    const newGift=[];
+    for (let i = 0; i < gifts.length; i++) {
+      newGift[i]=`Thank you, ${gifts[i]}, for the wonderful ${text} gift!`; }
+  
+    return newGift;}
+  
+  function countDown(number){
+    let counter = 0;
+    while (counter <= number) {
+      console.log(number--);} }
+  
